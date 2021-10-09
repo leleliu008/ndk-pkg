e15c83de93ccbe5963d76abab268d07b8645537a
leleliu008 <leleliu008@gmail.com>
2021-10-09 20:04:14 +0800

    publish new version 0.1.5

--- install.sh
+++ install.sh
@@ -2494,7 +2494,7 @@ main() {
         die "$INSTALL_BIN_FILENAME is already installed at $(command -v $INSTALL_BIN_FILENAME)"
     fi
 
-    RELEASE_VERSION='0.1.4'
+    RELEASE_VERSION='0.1.5'
     RELEASE_FILENAME="$INSTALL_BIN_FILENAME-${RELEASE_VERSION}.tar.gz"
 
     if [ "$COUNTRY" = china ] ; then
