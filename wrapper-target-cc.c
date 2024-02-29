da7925920b91c56819f6271a0ee18224fcaf7d52
leleliu008 <leleliu008@gmail.com>
2024-02-29 23:59:42 +0800

    optimized
    Signed-off-by: leleliu008 <leleliu008@gmail.com>

--- wrapper-target-cc.c
+++ wrapper-target-cc.c
@@ -10,32 +10,6 @@
 #define ACTION_CREATE_DYNAMICALLY_LINKED_EXECUTABLE 4
 
 int main(int argc, char * argv[]) {
-    const char * options[6] = { "-shared", "-static", "--static", "-pie", "-c", "-o" };
-          int    indexes[6] = {    -1,         -1,        -1,       -1,    -1,   -1  };
-
-    for (int i = 1; i < argc; i++) {
-        for (int j = 0; j < 6; j++) {
-            if (strcmp(argv[i], options[j]) == 0) {
-                indexes[j] = i;
-                break;
-            }
-        }
-    }
-
-    int action = 0;
-
-    if (indexes[0] > 0) {
-        // if -shared option is specified, then remove -static , --static , -pie options if they also are specified
-        action = ACTION_CREATE_SHARED_LIBRARY;
-    } else if ((indexes[1] > 0) || (indexes[2] > 0)) {
-        // if -shared option is not specified, but -static or --static option is specified, then remove -pie , -Wl,-Bdynamic option if it also is specified
-        action = ACTION_CREATE_STATICALLY_LINKED_EXECUTABLE;
-    } else if (indexes[3] > 0) {
-        action = ACTION_CREATE_DYNAMICALLY_LINKED_EXECUTABLE;
-    }
-
-    /////////////////////////////////////////////////////////////////
-
     char * const cc = getenv("ANDROID_NDK_CC");
 
     if (cc == NULL) {
@@ -62,6 +36,22 @@ int main(int argc, char * argv[]) {
         return 4;
     }
 
+    /////////////////////////////////////////////////////////////////
+
+    const char * const SYSROOT = getenv("ANDROID_NDK_SYSROOT");
+
+    if (SYSROOT == NULL) {
+        fprintf(stderr, "ANDROID_NDK_SYSROOT environment variable is not set.\n");
+        return 5;
+    }
+
+    if (SYSROOT[0] == '\0') {
+        fprintf(stderr, "ANDROID_NDK_SYSROOT environment variable value should be a non-empty string.\n");
+        return 6;
+    }
+
+    /////////////////////////////////////////////////////////////////
+
     size_t targetArgLength = strlen(TARGET) + 10U;
     char   targetArg[targetArgLength];
 
@@ -69,22 +59,10 @@ int main(int argc, char * argv[]) {
 
     if (ret < 0) {
         perror(NULL);
-        return 5;
+        return 7;
     }
 
     /////////////////////////////////////////////////////////////////
-
-    const char * const SYSROOT = getenv("ANDROID_NDK_SYSROOT");
-
-    if (SYSROOT == NULL) {
-        fprintf(stderr, "ANDROID_NDK_SYSROOT environment variable is not set.\n");
-        return 6;
-    }
-
-    if (SYSROOT[0] == '\0') {
-        fprintf(stderr, "ANDROID_NDK_SYSROOT environment variable value should be a non-empty string.\n");
-        return 7;
-    }
 
     size_t sysrootArgLength = strlen(SYSROOT) + 11U;
     char   sysrootArg[sysrootArgLength];
@@ -98,28 +76,48 @@ int main(int argc, char * argv[]) {
 
     /////////////////////////////////////////////////////////////////
 
-    char* argv2[argc + 4];
+    const char * options[6] = { "-shared", "-static", "--static", "-pie", "-c", "-o" };
+          int    indexes[6] = {    -1,         -1,        -1,       -1,    -1,   -1  };
 
-    argv2[0] = cc;
-    argv2[1] = targetArg;
-    argv2[2] = sysrootArg;
-
-    if (action == ACTION_CREATE_SHARED_LIBRARY) {
-        argv2[3] = (char*)"-fPIC";
-
-        for (int i = 1; i < argc; i++) {
-            if (strcmp(argv[i], "-static") == 0) {
-                argv2[i + 3] = (char*)"-fPIC";
-            } else if (strcmp(argv[i], "--static") == 0) {
-                argv2[i + 3] = (char*)"-fPIC";
-            } else if (strcmp(argv[i], "-pie") == 0) {
-                argv2[i + 3] = (char*)"-fPIC";
-            } else {
-                argv2[i + 3] = argv[i];
+    for (int i = 1; i < argc; i++) {
+        for (int j = 0; j < 6; j++) {
+            if (strcmp(argv[i], options[j]) == 0) {
+                indexes[j] = i;
+                break;
             }
         }
+    }
 
-        char sonameArg[100]; sonameArg[0] = '\0';
+    int action = 0;
+
+    if (indexes[0] > 0) {
+        // if -shared option is specified, then remove -static , --static , -pie options if they also are specified
+        action = ACTION_CREATE_SHARED_LIBRARY;
+    } else if ((indexes[1] > 0) || (indexes[2] > 0)) {
+        // if -shared option is not specified, but -static or --static option is specified, then remove -pie , -Wl,-Bdynamic option if it also is specified
+        action = ACTION_CREATE_STATICALLY_LINKED_EXECUTABLE;
+    } else if (indexes[3] > 0) {
+        action = ACTION_CREATE_DYNAMICALLY_LINKED_EXECUTABLE;
+    }
+
+    /////////////////////////////////////////////////////////////////
+
+    char* argv2[argc + 5];
+
+    char sonameArg[100]; sonameArg[0] = '\0';
+
+    if (action == ACTION_CREATE_SHARED_LIBRARY) {
+        for (int i = 1; i < argc; i++) {
+            if (strcmp(argv[i], "-static") == 0) {
+                argv2[i] = (char*)"-fPIC";
+            } else if (strcmp(argv[i], "--static") == 0) {
+                argv2[i] = (char*)"-fPIC";
+            } else if (strcmp(argv[i], "-pie") == 0) {
+                argv2[i] = (char*)"-fPIC";
+            } else {
+                argv2[i] = argv[i];
+            }
+        }
 
         char * outputFilePath = NULL;
         char * outputFileName = NULL;
@@ -194,6 +192,34 @@ int main(int argc, char * argv[]) {
 
             regfree(&regex);
         }
+    } else if (action == ACTION_CREATE_STATICALLY_LINKED_EXECUTABLE) {
+        for (int i = 1; i < argc; i++) {
+            if (strcmp(argv[i], "-rdynamic") == 0) {
+                argv2[i] = (char*)"-static";
+            } else if (strcmp(argv[i], "-Wl,--export-dynamic") == 0) {
+                argv2[i] = (char*)"-static";
+            } else if (strcmp(argv[i], "-Wl,-Bdynamic") == 0) {
+                argv2[i] = (char*)"-static";
+            } else if (strcmp(argv[i], "-pie") == 0) {
+                argv2[i] = (char*)"-static";
+            } else {
+                argv2[i] = argv[i];
+            }
+        }
+    } else {
+        for (int i = 1; i < argc; i++) {
+            argv2[i] = argv[i];
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////
+
+    argv2[0]        = cc;
+    argv2[argc]     = targetArg;
+    argv2[argc + 1] = sysrootArg;
+
+    if (action == ACTION_CREATE_SHARED_LIBRARY) {
+        argv2[argc + 2] = (char*)"-fPIC";
 
         if (sonameArg[0] == '\0') {
             argv2[argc + 3] = NULL;
@@ -201,29 +227,11 @@ int main(int argc, char * argv[]) {
             argv2[argc + 3] = sonameArg;
             argv2[argc + 4] = NULL;
         }
-    } else if (action == ACTION_CREATE_STATICALLY_LINKED_EXECUTABLE) {
-        for (int i = 1; i < argc; i++) {
-            if (strcmp(argv[i], "-rdynamic") == 0) {
-                argv2[i + 2] = (char*)"-static";
-            } else if (strcmp(argv[i], "-Wl,--export-dynamic") == 0) {
-                argv2[i + 2] = (char*)"-static";
-            } else if (strcmp(argv[i], "-Wl,-Bdynamic") == 0) {
-                argv2[i + 2] = (char*)"-static";
-            } else if (strcmp(argv[i], "-pie") == 0) {
-                argv2[i + 2] = (char*)"-static";
-            } else {
-                argv2[i + 2] = argv[i];
-            }
-        }
-
-        argv2[argc + 2] = NULL;
     } else {
-        for (int i = 1; i < argc; i++) {
-            argv2[i + 2] = argv[i];
-        }
-
         argv2[argc + 2] = NULL;
     }
+
+    /////////////////////////////////////////////////////////////////
 
     execv (cc, argv2);
     perror(cc);
