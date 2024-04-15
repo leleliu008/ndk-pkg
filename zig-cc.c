431a32cb0ba0ef9965d39edcc4de16c9bbd6d91f
leleliu008 <leleliu008@gmail.com>
2024-04-15 17:34:14 +0800

    optimized
    Signed-off-by: leleliu008 <leleliu008@gmail.com>

---
+++ zig-cc.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char * argv[]) {
+    char * const zig = getenv("ZIG");
+
+    if (zig == NULL) {
+        fprintf(stderr, "ZIG environment variable is not set.\n");
+        return 1;
+    }
+
+    if (zig[0] == '\0') {
+        fprintf(stderr, "ZIG environment variable value should be a non-empty string.\n");
+        return 2;
+    }
+
+    char* argv2[argc + 2];
+
+    argv2[0] = zig;
+    argv2[1] = (char*)"cc";
+    argv2[argc+1] = NULL;
+
+    for (int i = 1; i < argc; i++) {
+        argv2[i+1] = argv[i];
+    }
+
+    execv (zig, argv2);
+    perror(zig);
+    return 255;
+}
