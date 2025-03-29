60f68b411940f4e1b5134cdd9aceaa55a70ce8ca
leleliu008 <leleliu008@gmail.com>
2025-03-29 15:58:54 +0800

    optimized

--- phases.svg.sh
+++ phases.svg.sh
@@ -1,2 +1,2 @@
 ~/.ndk-pkg/uppm/installed/dot_static/bin/dot_static -Tsvg -o phases.svg phases.dot
-~/.ppkg/uppm/installed/d2/bin/d2 phases.dot phases.svg
+~/.ndk-pkg/uppm/installed/d2/bin/d2 phases.dot phases.svg
