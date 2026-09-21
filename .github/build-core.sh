531928282688bc0698f09da7a1c428f64f76619d
leleliu008 <leleliu008@gmail.com>
2026-09-21 18:56:58 +0800

    optimized

--- .github/build-core.sh
+++ .github/build-core.sh
@@ -73,13 +73,13 @@ case $2 in
         ./ndk-pkg setup
         ./ndk-pkg update
 
-        ./ndk-pkg install $2/elftool --static
+        ./ndk-pkg install $2/elftool
         ./ndk-pkg bundle  $2/elftool .tar.xz
 
-        ./ndk-pkg install $2/uppm@0.15.4 --static
+        ./ndk-pkg install $2/uppm@0.15.4
         ./ndk-pkg bundle  $2/uppm@0.15.4 .tar.xz
 
-        ./ndk-pkg install $2/ndk-pkg-core --static -I .
+        ./ndk-pkg install $2/ndk-pkg-core -I .
         ./ndk-pkg bundle  $2/ndk-pkg-core .tar.xz
 esac
 
